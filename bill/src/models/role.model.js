3e4fda1483d91619869700ff466ead3bd771d893
mohak51 <72740889+mohak51@users.noreply.github.com>
2025-04-03 12:43:40 +0530

    Add bill/src/models/role.model.js 1743664419347

---
+++ bill/src/models/role.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const roleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    permissions: {
+        type: [String],
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Role = mongoose.model('Role', roleSchema);
+
+module.exports = Role;
